9131ec092cda90c07658fd5b03f7cce8a5200c61
Somasys Desenvolvimento <trabalho.kaue@hotmail.com>
2022-07-14 14:28:32 -0300

    http

--- tutorial/10-fs_sync.js
+++ tutorial/10-fs_sync.js
@@ -1,5 +1,7 @@
 const {readFileSync, writeFileSync} = require('fs')
 
+console.log(`Start`);
+
 const first = readFileSync('./content/first.txt', 'utf8')
 const second = readFileSync('./content/second.txt', 'utf8')
 
@@ -7,4 +9,7 @@ writeFileSync(`./content/result-sync.txt`,
     `Here is the result: ${first}, ${second}`,
     {flag: 'a'}
     )
+
+console.log(`Done with this task`);
+console.log(`Start the next one`);
 
